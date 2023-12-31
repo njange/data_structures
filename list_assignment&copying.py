d4b02c1d3c5fdff255a8e76fd3c39c795340e110
Githuo Njange <128843179+njange@users.noreply.github.com>
2023-12-31 19:58:59 +0300

    Learning about lists,tuples and their operations

--- list_assignment&copying.py
+++ list_assignment&copying.py
@@ -1,15 +1,15 @@
-list1=[10,2,3.4,5.1,6,7,8,9]
-list2=list1
-list1[0]="hello"
+list1 = [10, 2, 3.4, 5.1, 6, 7, 8, 9]
+list2 = list1
+list1[0] = "hello"
 print(list1)
 print(list2)
 
 
-list3=list1.copy()
+list3 = list1.copy()
 print(list3)
-list1[0]="bye"
+list1[0] = "bye"
 print(list1)
 
-result= [x**2 for x in range(10)]
+result = [x**2 for x in range(10)]
 print(result)
 
