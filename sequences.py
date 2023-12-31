2bc82c4de1c272a08c12dcf902f0b6ffcfd97aae
Githuo Njange <128843179+njange@users.noreply.github.com>
2023-12-31 19:56:24 +0300

    Learning about lists,tuples and their operations

--- sequences.py
+++ sequences.py
@@ -2,37 +2,37 @@ numbers = [10,20,5,7,1,3]
 numbers2 = (8,3,9,2,5)
 name = "John"
 
-#Length operation
+# Length operation
 
 print(len(name))
 print(len(numbers))
 print(len(numbers2))
 
-#Length operation is used to find the number of elements in the sequence
+# Length operation is used to find the number of elements in the sequence
 
 #Select operation
 print(numbers[0])
 print(numbers2[1])
 print(name[2])
-#Select operation is used to select a single element
+# Select operation is used to select a single element
 
-#Slice operation
+# Slice operation
 print(numbers[0:3])
 print(numbers2[1:4])
 print(name[2:4])
-#Slice operation is used to select a range of elements
+# Slice operation is used to select a range of elements
 
-#Count operation
+# Count operation
 print(numbers.count(5))
 print(numbers2.count(5))
 print(name.count("h"))
-#Count operation is used to count the number of occurences of the element
+# Count operation is used to count the number of occurences of the element
 
-#Index operation
+# Index operation
 print(numbers.index(5))
 print(numbers2.index(5))
 print(name.index("h"))
-#Index operation is used to find the index of the first occurence of the element.
+# Index operation is used to find the index of the first occurence of the element.
 
 
 #Membership operation
