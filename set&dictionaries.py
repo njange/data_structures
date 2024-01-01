18bb76baff556995671a504d2ddb478a449c6413
Githuo Njange <128843179+njange@users.noreply.github.com>
2024-01-01 12:05:47 +0300

    Learning about lists,tuples and their operations

---
+++ set&dictionaries.py
@@ -0,0 +1,15 @@
+dictionary = {'name': 'Chris', 'city': 'Seattle', 'cake': 'Chocolate'}
+print(dictionary)
+
+dictionary2 = {"j": 1, "k": 2, "l": 3}
+print(dictionary2)
+
+dictionary3 = dict([("m", 4), ("n", 5), ("o", 6)])
+print(dictionary3)
+
+dictionary4 = {}
+dictionary4["p"] = 7
+print(dictionary4)
+
+# Sets
+
