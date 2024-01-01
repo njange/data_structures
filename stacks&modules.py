3640a7b153aa60e523d57d6e64fe205df02ba805
Githuo Njange <128843179+njange@users.noreply.github.com>
2024-01-01 12:46:14 +0300

    Learning about stacks using modules; queue and collections

---
+++ stacks&modules.py
@@ -0,0 +1,20 @@
+import collections
+stack = collections.deque()
+stack.append(10)
+stack.append(20)
+stack.append(30)
+print(stack)
+
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+
+# Using queue module
+import queue
+stack = queue.LifoQueue(3)
+stack.put(10)
+stack.put(20)
+stack.put(30)
+print(stack)
+
+print(stack.get())
