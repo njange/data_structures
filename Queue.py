2d6b82d947c3fe4e7732fe20931ec9bc4eed136d
Githuo Njange <128843179+njange@users.noreply.github.com>
2024-01-05 12:53:08 +0300

    Learning about stacks using modules; queue and collections

---
+++ Queue.py
@@ -0,0 +1,3 @@
+# FIFO
+# Just like a real world queue, queue data structure allows insertion of an element at one end and deletion of an element at the other end.
+# This queue is called First In First Out (FIFO) queue because the element which is inserted first is retrieved first.
