1b4d1020ecb320864260b93a53c684c05d5cf519
Githuo Njange <128843179+njange@users.noreply.github.com>
2024-01-06 16:36:29 +0300

    Learning about stacks using modules; queue and collections

--- Queue.py
+++ Queue.py
@@ -17,3 +17,13 @@ print(queue)
 print(queue.pop(0))
 print(queue.pop(0))
 print(queue.pop(0))
+
+queue.insert(0, 10)
+queue.insert(0, 20)
+queue.insert(0, 30)
+print(queue)
+
+queue.pop()
+print(queue)
+
+print(not queue)
