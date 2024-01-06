212ac5e8130f043961dee0b00e7895d3d303ea67
Githuo Njange <128843179+njange@users.noreply.github.com>
2024-01-06 16:42:25 +0300

    Learning about stacks using modules; queue and collections

--- Queue.py
+++ Queue.py
@@ -27,3 +27,39 @@ queue.pop()
 print(queue)
 
 print(not queue)
+
+queue.append(40)
+print(queue)
+
+print(queue[0])
+
+queue1 = []
+def enqueue():
+    element = input("Enter the element to be inserted: ")
+    queue1.append(element)
+    print(element, "is inserted into the queue")
+
+def dequeue():
+    if not queue1:
+        print("Queue is empty")
+    else:
+        e = queue1.pop(0)
+        print("Removed element: ", e)
+
+def display():
+    print(queue1)
+
+while True:
+    print("Select the operation 1. Enqueue 2. Dequeue 3. Display 4. Exit")
+    choice = int(input())
+    if choice == 1:
+        enqueue()
+    elif choice == 2:
+        dequeue()
+    elif choice == 3:
+        display()
+    elif choice == 4:
+        break
+    else:
+        print("Enter a valid choice")
+
