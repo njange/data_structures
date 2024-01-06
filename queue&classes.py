a96466f03a1c851cff5e72ff8d9b39e4618380ca
Githuo Njange <128843179+njange@users.noreply.github.com>
2024-01-06 18:11:14 +0300

    Learning about stacks using modules; queue and collections

--- queue&classes.py
+++ queue&classes.py
@@ -30,3 +30,26 @@ print(q1.get())
 print(q1.get())
 
 # Priority Queue
+q2 = []
+q2.append(2)
+q2.append(1)
+q2.append(3)
+print(q2)
+print(q2.sort())
+print(q2)
+
+q3 = queue.PriorityQueue()
+q3.put(10)
+q3.put(20)
+q3.put(30)
+print(q3)
+
+print(q3.get())
+
+q4 = []
+q4.append((2, "work"))
+q4.append((1, "eat"))
+q4.append((3, "sleep"))
+print(q4)
+print(q4.sort())
+print(q4)
