e67515df262c267a4aba97424bc67249f24484ae
Githuo Njange <128843179+njange@users.noreply.github.com>
2024-01-09 20:51:27 +0300

    Learning about linked lists

--- Linked lists/singly linked list.py
+++ Linked lists/singly linked list.py
@@ -3,5 +3,29 @@
 # 2. Deletion
 # 3. Traversal
 
+# create a node
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.ref = None
+
+
+# create a linked list
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def print_LL(self):
+        if self.head is None:
+            print("Linked list is empty")
+        else:
+            n = self.head
+            while n is not None:
+                print(n.data)
+                n = n.ref
+
+
 
 
